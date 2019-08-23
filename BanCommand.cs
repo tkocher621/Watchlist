34d3d8523f6c2c829d98cbd24318ad4cadf02223
tkocher621 <42099068+tkocher621@users.noreply.github.com>
2019-08-23 18:21:24 -0400

    Add files via upload

---
+++ BanCommand.cs
@@ -0,0 +1,70 @@
+﻿using Smod2.Commands;
+using System.IO;
+
+namespace Watchlist
+{
+	// This is the file to initialize a command into the game
+
+	class BanCommand : ICommandHandler
+	{
+		public string GetCommandDescription()
+		{
+			return "Bans a player from using the report system.";
+		}
+
+		public string GetUsage()
+		{
+			return "WBAN (STEAMID)";
+		}
+
+		public string[] OnCall(ICommandSender sender, string[] args)
+		{
+			// Verify the correct directories exist
+			if (Directory.Exists(Plugin.ReportBanFolder))
+			{
+				if (args.Length > 0)
+				{
+					// If so, parse the SteamID64 as a long
+					if (long.TryParse(args[0], out long steamid))
+					{
+						string file = $"{Plugin.ReportBanFolder}{Path.DirectorySeparatorChar}{steamid}.txt";
+						if (!File.Exists(file))
+						{
+							// If the player is not already banned, create a file under their name to ban them
+							File.Create(file);
+							return new string[]
+							{
+							"Player successfully banned."
+							};
+						}
+						else
+						{
+							return new string[]
+							{
+							"Player is already banned."
+							};
+						}
+					}
+					else
+					{
+						return new string[]
+						{
+						"Error parsing SteamID."
+						};
+					}
+				}
+				else
+				{
+					return new[] { GetUsage() };
+				}
+			}
+			else
+			{
+				return new string[]
+				{
+					"Error locating config folder."
+				};
+			}
+		}
+	}
+}
---
+++ ReconnectCommand.cs
@@ -0,0 +1,47 @@
+﻿using Smod2.Commands;
+using System.Net.Sockets;
+
+namespace Watchlist
+{
+	// This command reconnects the plugin to the server if it happens to get disconnected for any reason
+
+	class ReconnectCommand : ICommandHandler
+	{
+		private Plugin instance;
+
+		public ReconnectCommand(Plugin plugin)
+		{
+			instance = plugin;
+		}
+
+		public string GetCommandDescription()
+		{
+			return "Reconnects the client to the discord bot.";
+		}
+
+		public string GetUsage()
+		{
+			return "WRECONNECT";
+		}
+
+		public string[] OnCall(ICommandSender sender, string[] args)
+		{
+			EventHandler.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			EventHandler.socket.Connect("127.0.0.1", 9090);
+			if (EventHandler.IsConnected())
+			{
+				return new string[]
+				{
+					"Successfully reconnected."
+				};
+			}
+			else
+			{
+				return new string[]
+				{
+					"Failed to reconnect."
+				};
+			}
+		}
+	}
+}
